f2a7a39f37fc48fde730fbd598181bd2d707325f
omaus <omaus@bio.uni-kl.de>
2022-05-12 19:24:16 +0200

    Add Markdown parsing script (WIP)

---
+++ simpleParseScriptMd.fsx
@@ -0,0 +1,26 @@
+open System.IO
+
+let inputPath = Path.Combine(__SOURCE_DIRECTORY__, "TestResults.xml")
+let outputPath = Path.Combine(__SOURCE_DIRECTORY__, "README.md")
+
+let input = File.ReadAllText inputPath
+
+let parsedInput = 
+    [|
+        for c in input do
+            match c with 
+            | '<' -> yield! "&lt"
+            | '>' -> yield! "&gt"
+            | '\n' -> yield! "</p><p>"
+            | _ -> yield c
+    |]
+    |> System.String
+
+let res =
+    """# Unit Test Result:  
+    <p>
+    """
+    + parsedInput +
+    """</p>"""
+
+File.WriteAllText(outputPath, res)
