79970528b64ed0dbd16cc388b4acb3d0d7f353e6
omaus <omaus@bio.uni-kl.de>
2022-05-16 17:52:59 +0200

    Upate parsing for Markdown instead of HTML

--- simpleParseScriptMd.fsx
+++ simpleParseScriptMd.fsx
@@ -1,26 +1,41 @@
+#r "nuget: FSharpAux"
+
 open System.IO
+open FSharpAux
 
 let inputPath = Path.Combine(__SOURCE_DIRECTORY__, "TestResults.xml")
 let outputPath = Path.Combine(__SOURCE_DIRECTORY__, "README.md")
 
 let input = File.ReadAllText inputPath
 
-let parsedInput = 
-    [|
-        for c in input do
-            match c with 
-            //| '<' -> yield! "&lt"
-            //| '>' -> yield! "&gt"
-            | '\n' -> yield! "</p><p>"
-            | _ -> yield c
-    |]
-    |> System.String
+let splittedInput = String.split '>' input
+
+let rec indent level i (oldArr : string []) newArr =
+    if i < oldArr.Length - 1 then
+        let line = oldArr.[i]
+        let spaces = String.init level (fun _ -> " ")
+        let newLevel = 
+            if String.contains "/>" line then level - 1 
+            elif String.contains ">" line && String.contains "/>" line |> not then level + 1
+            else level
+        let newNewArr = [|yield! newArr; $"{spaces}{line}"|]
+        indent newLevel (i + 1) oldArr newNewArr
+    else 
+        let line = oldArr.[i]
+        let spaces = String.init level (fun _ -> " ")
+        let newLevel = 
+            if String.contains "/>" line then level - 1 
+            elif String.contains ">" line && String.contains "/>" line |> not then level + 1
+            else level
+        [|yield! newArr; $"{spaces}{line}"|]
+
+let parsedInput = indent 0 0 splittedInput [||]
+
+let concatinatedParsedInput = parsedInput |> String.concat "\n"
 
 let res =
     """# Unit Test Result:  
-    <p>
     """
-    + parsedInput +
-    """</p>"""
+    + concatinatedParsedInput
 
 File.WriteAllText(outputPath, res)
